aeb0d7a12c16a357d428f0cdb6f5308a866855fd
ryakina <dr130801@gmail.com>
2021-04-06 02:35:36 +0300

    1

--- demo_app.py
+++ demo_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 with st.echo(code_location='below'):
-    st.title("Hello, World???")
+    st.title("Hello, World!!??")
     """
     This is a test.
     """
